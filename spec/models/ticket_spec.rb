cdac42e69e8c11062d31a468183658adbfa29627
Brandon Nelson <nelsonb3@oregonstate.edu>
2024-01-17 16:49:24 -0800

    ticket_spec.rb initial attributes

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -7,4 +7,28 @@ RSpec.describe Ticket, type: :model do
         expect(Ticket).to_not be_nil
     end
 
+    it "has name" do
+        expect(Ticket).to respond_to(:name)
+    end
+
+    it "has description" do
+        expect(Ticket).to respond_to(:description)
+    end
+
+    it "has phone" do
+        expect(Ticket).to respond_to(:phone)
+    end
+
+    it "has organization id" do
+        expect(Ticket).to respond_to(:organization_id)
+    end
+
+    it "has resource categort id" do
+        expect(Ticket).to respond_to(:resource_category_id)
+    end
+
+    it "has region id" do
+        expect(Ticket).to respond_to(:region_id)
+    end
+
 end
