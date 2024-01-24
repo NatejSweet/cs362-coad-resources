d71afee6a841821e3e09e24b241e588ce3ddc3e1
Brandon Nelson <nelsonb3@oregonstate.edu>
2024-01-24 15:07:44 -0800

    added password token, reset_password_toke, and confirmation_token assertions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,22 @@ RSpec.describe User, type: :model do
         expect(user).to respond_to(:email)
     end
 
+    it "has an encrypted_password" do
+        expect(user).to respond_to(:encrypted_password)
+    end
+
+    it "has a reset_password_token" do
+        expect(user).to respond_to(:reset_password_token)
+    end
+
+    it "has a confirmation_token" do
+        expect(user).to respond_to(:confirmation_token)
+    end
+
+    it "has an unconfirmed_email" do
+        expect(user).to respond_to(:unconfirmed_email)
+    end
+
     it "has a role" do
         expect(user).to respond_to(:role)   
     end
