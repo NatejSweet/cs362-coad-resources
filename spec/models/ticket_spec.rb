42661ceee9676dc97dfb152dcf36c303777cc5e2
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-02-06 17:14:47 -0800

    add validate and scope tests for ticket_spec.rb(actual spec commit)

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -30,21 +30,63 @@ RSpec.describe Ticket, type: :model do
     it "has region id" do
         expect(ticket).to respond_to(:region_id)
     end
-
+    
+#belongs_to's
     it {should belong_to(:region)} 
     it {should belong_to(:resource_category)} 
     it {should belong_to(:organization)} 
+#end of belongs_to's
 
+#validations   
     it "validates at least one name" do
         expect(ticket).to validate_length_of(:name).is_at_least(1)
     end
 
+    it "validates at most 1020 descriptions" do
+      expect(ticket).to validate_length_of(:description).is_at_most(1020)
+    end
+
+    describe "validates phone numbers" do
+      it {should allow_value("+1-555-555-1212").for(:phone)} 
+      it { should_not allow_value("popcorn").for(:phone)}
+    end
+
+    it "validates phone using phony_plausible " do
+      expect(Ticket.validators_on(:phone)).to include(PhonyPlausibleValidator)
+    end
+#end of validations
+
+#scopes
     it "responds to open with true" do
         expect(ticket.open?).to be_truthy
     end
 
+    it "responds to captured with true" do     
+      expect(ticket.captured?).to_not be_truthy
+    end
     it "responds to Ticket.closed with an array of closed tickets" do
         expect(Ticket.closed).to eq([])
     end
 
-end
+    it "responds to all_organization with an array of all organization tickets that are open" do
+        expect(Ticket.all_organization).to eq([]) and expect(Ticket.all_organization).to_not eq(nil)
+    end
+    
+    it "responds to organization with an array of organization tickets that are open and have a valid organization id" do
+      expect(Ticket.organization(1)).to eq([]) and expect(Ticket.organization(1)).to_not eq(nil)
+    end
+
+    it "responds to closed_organization with an array of organization tickets that are closed and have a valid organization id" do
+      expect(Ticket.closed_organization(1)).to eq([]) and expect(Ticket.closed_organization(1)).to_not eq(nil)
+    end
+
+    it "responds to region with an array of tickets that have a valid region id" do
+      expect(Ticket.region(1)).to eq([]) and expect(Ticket.region(1)).to_not eq(nil)
+    end
+
+    it "responds to resource_category with an array of tickets that have a valid resource category id" do
+      expect(Ticket.resource_category(1)).to eq([]) and expect(Ticket.resource_category(1)).to_not eq(nil)
+    end
+#end of scopes
+
+  end
