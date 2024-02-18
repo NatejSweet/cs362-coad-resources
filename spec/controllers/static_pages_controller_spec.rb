4abd3491921370c5f535d23fc23332404b71a75c
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-02-18 15:32:20 -0800

    finish static_pages_controller tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
+  describe "GET #index" do
+    it "should be successful" do 
+      get(:index)
+      expect(response).to be_successful 
+    end
+  end
 end
