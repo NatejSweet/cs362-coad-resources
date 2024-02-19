6386197c116a1dc07139ba9cb9039b2c1c71e233
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-02-18 21:03:27 -0800

    update phone number in ticket factory to vaild format

--- spec/factories/ticket_factory.rb
+++ spec/factories/ticket_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :ticket do
       name { "Test Ticket" }
       description { "Test Description" }
-      phone { "541-111-1111" }
+      phone { "+1-555-555-1212" }
       region_id { 1 }
       resource_category_id { 1 }
       organization_id { 1 }
