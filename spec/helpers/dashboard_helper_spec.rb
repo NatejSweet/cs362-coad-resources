fea577281687a3a05fb2c0f468526284e9c91c84
BigSkyCoder2003 <danielwlouns@gmail.com>
2024-02-19 22:49:55 +0000

    finish dashboard_helper

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the DashboardHelper. For example:
-#
-# describe DashboardHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe DashboardHelper, type: :helper do
+  describe 'test dashboard_for' do
+    it 'should return admin_dashboard' do
+      user = double('User', admin?: true, organization: nil)
+      expect(helper.dashboard_for(user)).to eq('admin_dashboard')
+    end
 
+    it 'should return organization_submitted_dashboard' do
+      user = double('User', admin?: false, organization: double('Organization', submitted?: true, approved?: false))
+      expect(helper.dashboard_for(user)).to eq('organization_submitted_dashboard')
+    end
+
+    it 'should return organization_approved_dashboard' do
+      user = double('User', admin?: false, organization: double('Organization', submitted?: false, approved?: true))
+      expect(helper.dashboard_for(user)).to eq('organization_approved_dashboard')
+    end
+
+    it 'should return create_application_dashboard' do
+      user = double('User', admin?: false, organization: double('Organization', submitted?: false, approved?: false))
+      expect(helper.dashboard_for(user)).to eq('create_application_dashboard')
+    end
+  end
 end
