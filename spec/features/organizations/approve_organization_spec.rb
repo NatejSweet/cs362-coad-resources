64e658f1b0c302e9c3af2d660691c6cb426f2b8e
Nathan Sweet <nathan.j.sweet852@gmail.com>
2024-02-26 14:44:53 -0800

    safety push

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+    before do 
+        @admin = create(:user, :admin)
+        @organization = create(:organization)
+        log_in_as(@admin)
+    end
+    it 'approves an organization' do
+        visit organization_path(@organization.id)
+        click_link 'Approve'
+        expect(page).to have_content('approved')
+    end
 
 end
